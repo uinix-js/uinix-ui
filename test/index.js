b0453f549ddeb76563b2193749888e26633e0fc8
chrisrzhou <chrisrzhou@pm.me>
2021-04-20 21:50:42 -0700

    feat: support options.responsiveAttributes and negative theme values

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
 export * from './es.js';
+export * from './renderer-plugins/index.js';
 export * from './styles/index.js';
 export * from './util/index.js';
---
+++ test/renderer-plugins/theme-value.js
@@ -0,0 +1,5 @@
+import test from 'tape';
+
+test('themeValue', (t) => {
+  t.end();
+});
