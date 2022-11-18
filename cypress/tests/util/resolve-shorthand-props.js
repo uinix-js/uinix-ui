25eeed96f07762c7917390458fe589624783ae75
chrisrzhou <chrisrzhou@pm.me>
2022-11-17 23:37:41 -0800

    refactor: cleanup `resolveShorthandProps` tests

--- cypress/tests/util/resolve-shorthand-props.js
+++ cypress/tests/util/resolve-shorthand-props.js
@@ -1,86 +1,67 @@
 import {resolveShorthandProps} from '../../../lib/util/resolve-shorthand-props.js';
 
 describe('resolveShorthandProps', () => {
-  const shorthandPropsMapping = {
-    margin: ['m'],
-    padding: ['p'],
-  };
-  const props = {a: 42, b: true};
-
-  const emptyProps = {};
-  const emptyShorthandPropsMapping = {};
-
-  it('should resolve empty props with empty shorthandPropsMapping returning empty restProps and empty shorthandStyle', () => {
-    expect(
-      resolveShorthandProps(emptyShorthandPropsMapping)(emptyProps),
-    ).to.deep.equal([{}, {}]);
+  it('should return empty restProps and empty shorthandStyle when provided with empty props and empty shorthandPropsMapping', () => {
+    expect(resolveShorthandProps({})({})).to.deep.equal([{}, {}]);
   });
 
-  it('should resolve non-empty props with empty shorthandPropsMapping returning provided props and empty shorthandStyle', () => {
-    expect(
-      resolveShorthandProps(emptyShorthandPropsMapping)(props),
-    ).to.deep.equal([props, {}]);
+  it('should return the provided props and empty shorthandStyle when provided with non-empty props and empty shorthandPropsMapping', () => {
+    expect(resolveShorthandProps({})({a: 42})).to.deep.equal([{a: 42}, {}]);
   });
 
-  it('should resolve empty props with non-empty shorthandPropsMapping returning empty props and empty shorthandStyle', () => {
-    expect(
-      resolveShorthandProps(shorthandPropsMapping)(emptyProps),
-    ).to.deep.equal([{}, {}]);
+  it('should return empty props and empty shorthandStyle when provided with empty props and non-empty shorthandPropsMapping', () => {
+    expect(resolveShorthandProps({margin: ['m']})({})).to.deep.equal([{}, {}]);
   });
 
-  it('should resolve non-empty props with non-empty shorthandPropsMapping returning provided props and empty shorthandStyle if no props are registered in shorthandPropsMapping', () => {
-    expect(resolveShorthandProps(shorthandPropsMapping)(props)).to.deep.equal([
-      props,
+  it('should return props and empty shorthandStyle when provided with non-empty props and non-empty shorthandPropsMapping without matching props', () => {
+    expect(resolveShorthandProps({margin: ['m']})({a: 42})).to.deep.equal([
+      {a: 42},
       {},
     ]);
   });
 
-  it('should resolve non-empty props with non-empty shorthandPropsMapping and return styleObject based on matching props defined in shorthandPropsMapping and returning other props in restProps', () => {
-    const shorthandPropsMapping = {
-      margin: ['m'],
-      padding: ['p'],
-    };
-    const props = {
-      a: 42,
-      b: true,
-      m: '42px',
-      p: '24px',
-    };
-
-    expect(resolveShorthandProps(shorthandPropsMapping)(props)).to.deep.equal([
+  it('should return shorthandStyle for matching props in shorthandPropsMapping and restProps', () => {
+    expect(
+      resolveShorthandProps({
+        margin: ['m'],
+        padding: ['p'],
+      })({
+        a: 42,
+        b: true,
+        m: '42px',
+        p: '24px',
+      }),
+    ).to.deep.equal([
       {a: 42, b: true},
       {margin: '42px', padding: '24px'},
     ]);
   });
 
-  it('should resolve and populate shorthandStyle in order of the precedence specified in the shorthandPropsMapping', () => {
-    const shorthandPropsMapping = {
-      margin: ['m'],
-      marginBottom: ['mb', 'my', 'm'],
-    };
-
+  it('should return shorthandStyle and restProps, with shorthandStyle resolved based on precedence in shorthandPropsMapping', () => {
     expect(
-      resolveShorthandProps(shorthandPropsMapping)({
-        a: 42,
-        b: true,
-        m: '42px',
-      }),
-    ).to.deep.equal([
-      {a: 42, b: true},
-      {margin: '42px', marginBottom: '42px'},
-    ]);
-
-    expect(
-      resolveShorthandProps(shorthandPropsMapping)({
+      resolveShorthandProps({
+        margin: ['m'],
+        marginBottom: ['mb', 'my', 'm'],
+        marginLeft: ['ml', 'mx', 'm'],
+        marginRight: ['mr', 'mx', 'm'],
+        marginTop: ['mt', 'my', 'm'],
+      })({
         a: 42,
         b: true,
         mb: '12px',
+        mx: '6px',
         my: '24px',
-        m: '42px',
+        m: '48px',
       }),
     ).to.deep.equal([
       {a: 42, b: true},
-      {margin: '42px', marginBottom: '12px'},
+      {
+        margin: '48px',
+        marginBottom: '12px',
+        marginLeft: '6px',
+        marginRight: '6px',
+        marginTop: '24px',
+      },
     ]);
   });
 });
